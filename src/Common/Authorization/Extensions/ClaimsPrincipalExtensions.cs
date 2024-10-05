0f531c765b30bcc6735c19213262462d18f0ac1a
danieldantasdev <contatodanieldantasdev@gmail.com>
2024-10-05 17:58:33 -0300

    init: commit initial

---
+++ src/Common/Authorization/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace Authorization.Extensions;
+
+/// <summary>
+/// Contains extension methods for the <see cref="ClaimsPrincipal"/> class.
+/// </summary>
+internal static class ClaimsPrincipalExtensions
+{
+    /// <summary>
+    /// Gets the identity provider identifier of the currently authenticated user.
+    /// </summary>
+    /// <param name="claimsPrincipal">The claims principal.</param>
+    /// <returns>The identity provider identifier of the currently authenticated user if it exists, or an empty string</returns>
+    internal static string GetIdentityProviderId(this ClaimsPrincipal claimsPrincipal) =>
+        claimsPrincipal.Claims.SingleOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value ?? string.Empty;
+}
---
+++ src/Common/Authorization/RequestClients/RequestClientConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Authorization.Contracts;
+using Infrastructure.EventBus;
+using MassTransit;
+
+namespace Authorization.RequestClients;
+
+/// <summary>
+/// Represents the request client configuration.
+/// </summary>
+internal sealed class RequestClientConfiguration : IRequestClientConfiguration
+{
+    /// <inheritdoc />
+    public void AddRequestClients(IRegistrationConfigurator registrationConfigurator) =>
+        registrationConfigurator.AddRequestClient<IUserPermissionsRequest>();
+}
